3c746c480bf8c027ccfcf0a3c0212d14d16d2bd3
zapsys <zapsyssolutions@gmail.com>
2025-10-04 21:02:07 -0300

    Capacitor app config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.zapsys.listadecompras',
+  appName: 'Lista de Compras',
+  webDir: 'dist'
+};
+
+export default config;
